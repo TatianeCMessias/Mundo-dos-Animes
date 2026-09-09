9c9f0901276b98248a820fdd79fdf1cfafc720c5
Tatiane Messias <tatianem600@gmail.com>
2026-09-08 22:02:08 -0300

    feat: ajuste na main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import Main from './Main/Main.jsx'
-import Header from'./Header/Header.jsx'
-import Footer from'./Footer/Footer.jsx'
+import './Main/Main.jsx'
+import './Header/Header.jsx'
+import './Footer/Footer.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Main />
-    <Header />
-    <Footer />
+    <App />
   </StrictMode>,
 )
